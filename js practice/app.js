b07751c64c118cfed94b05614b3ca48b21912e29
Mitch-devs <mitchell.roberts12@hotmail.com>
2024-10-21 10:59:58 +1000

    still writing computer logic for randomNum = rock, paper, scissors

--- js practice/app.js
+++ js practice/app.js
@@ -1,10 +1,18 @@
 // Write logic for computer to choose between rock, paper scissors
 // Write logic for human to choose " " " " 
-// if choice is equal to 0 - 0.33 choose rock, if else 0.33 - 0.66 choose paper, if else 0.67 - 1 choose scissors 
+// if choice is equal to 0 - 32 choose rock, if else 33 - 0.66 choose paper, else return scissors 
 
-let choice =["rock", "paper", "scissors"]
-
-function getComputerChoice() {
-    
+function randomNum(min,max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
-console.log(getComputerChoice);
+
+
+function getComputerChoice(min,max) { 
+    if (randomNum < 33) {
+        return "Rock!";
+    } else if (randomNum > 33 && randomNum < 66) {
+        return "Paper!";
+    } else return "Scissors!";
+}
+
+console.log (randomNum(0,100));
