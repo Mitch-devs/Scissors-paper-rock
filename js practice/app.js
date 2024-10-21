cd8b7218071564ce07c370ee34c515d9eea391a1
Mitch-devs <mitchell.roberts12@hotmail.com>
2024-10-21 10:16:03 +1000

    start of writing logic for rock paper scissors

--- js practice/app.js
+++ js practice/app.js
@@ -1,1 +1,10 @@
+// Write logic for computer to choose between rock, paper scissors
+// Write logic for human to choose " " " " 
+// if choice is equal to 0 - 0.33 choose rock, if else 0.33 - 0.66 choose paper, if else 0.67 - 1 choose scissors 
 
+let choice =["rock", "paper", "scissors"]
+
+function getComputerChoice() {
+    
+}
+console.log(getComputerChoice);
